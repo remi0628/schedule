26cae6d172a62309bbce3b312656e989773f77f2
REMI <safaia0628@yahoo.co.jp>
2016-12-01 03:06:53 +0900

    new file

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="stylesheet" type="text/css" href="main.css">
+		<title>schedule</title>
+	</head>
+	<body>
+		<div class="menu_list">
+			<ul class="menu">
+				<li class="menu"></li>
+				<li class="menu"></li>
+				<li class="menu"><button type="submit" name="login">login</button></li>
+			</ul>
+		</div>
+		<h1 class="home">スケジュール管理</h1>
+		<div class="choice">
+			<ul class="choice">
+				<li class="box"><button type="submit" name="decide">日程を決める</button></li>
+				<li class="box"><button type="submit" name="take">出欠を取る</button></li>
+			</ul>
+	</body>
+</html>
